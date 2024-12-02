776541e39f99dfcea9272fca11c3fdad08ba402e
hungphanquocviet <hungphanquocviet@gmail.com>
2024-12-02 08:25:54 -0700

    upload day 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,9 +1,31 @@
-import math
-filepath = './day1/sample.txt'
+filepath = './day2/sample.txt'
 
 a = []
-
 with open(filepath, encoding='utf8') as file:
     for line in file.readlines():
-        x, y = map(int, line.strip().split())
+        temp = list(map(int, line.split()))
+        a.append(temp)
 
+cnt = 0
+
+def check_array(arr):
+    if all(arr[i] < arr[i+1] for i in range(len(arr) - 1)):
+        return "inc"
+    elif all(arr[i] > arr[i+1] for i in range(len(arr) - 1)):
+        return "dec"
+    else:
+        return "no"
+    
+for code in a:
+    if check_array(code) == "no":
+        continue
+
+    safe = True
+    for i in range(1, len(code)):
+        if abs(code[i] - code[i-1]) > 3 or abs(code[i] - code[i-1]) < 1:
+            safe = False
+            break
+    if safe:
+        cnt += 1
+
+print(cnt)
--- day2/day2_2.py
+++ day2/day2_2.py
@@ -1,7 +1,38 @@
-import math
 filepath = './day2/sample.txt'
 
 a = []
 with open(filepath, encoding='utf8') as file:
     for line in file.readlines():
-        x, y = map(int, line.strip().split())
+        temp = list(map(int, line.split()))
+        a.append(temp)
+
+cnt = 0
+
+def is_safe(code):
+    increase = all(code[i] < code[i+1] for i in range(len(code) - 1))
+    decrease = all(code[i] > code[i+1] for i in range(len(code) - 1))
+
+    good = True
+    for i in range(1, len(code)):
+        if abs(code[i] - code[i-1]) > 3 or abs(code[i] - code[i-1]) < 1:
+            good = False
+            break
+    safe = (increase or decrease) and good
+    return safe
+
+def check_safety(code):
+    if is_safe(code):
+        return True
+    
+    for i in range(len(code)):
+        temp = code[:i] + code[i+1:]
+        if is_safe(temp):
+            return True
+    
+    return False
+
+for code in a:
+    if check_safety(code):
+        cnt += 1
+
+print(cnt)
